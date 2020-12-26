9e5199dab3570459de867960d9f81b3a0ca5d122
Sajjad <real.fisae@gmail.com>
2020-12-26 23:03:02 +0330

    Create Mystring class

---
+++ Mystring.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Mystring.cpp
+ * Author: sajjad
+ * 
+ * Created on December 26, 2020, 11:02 PM
+ */
+
+#include "Mystring.h"
+
+Mystring::Mystring() {
+}
+
+Mystring::Mystring(const Mystring& orig) {
+}
+
+Mystring::~Mystring() {
+}
+
---
+++ Mystring.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Mystring.h
+ * Author: sajjad
+ *
+ * Created on December 26, 2020, 11:02 PM
+ */
+
+#ifndef MYSTRING_H
+#define MYSTRING_H
+
+class Mystring {
+public:
+    Mystring();
+    Mystring(const Mystring& orig);
+    virtual ~Mystring();
+private:
+
+};
+
+#endif /* MYSTRING_H */
+
