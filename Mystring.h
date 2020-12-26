4a2bc93840879469c00b6c65093a8138b7531f4b
Sajjad <real.fisae@gmail.com>
2020-12-26 23:12:04 +0330

    Add three methods to Mystring class

--- Mystring.h
+++ Mystring.h
@@ -20,6 +20,11 @@ public:
     Mystring(char *s); // Overloaded constructor
     Mystring(const Mystring& orig); // copy constructor
     virtual ~Mystring(); // destructor
+    
+public:
+    void display() const; // display str using std::cout
+    int get_length() const; // returns length of str
+    const char get_str() const; // returns str
 private:
     char *str; // pointer to a char[] that holds a c-style string
 };
