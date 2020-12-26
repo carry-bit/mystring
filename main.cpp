7454032c96877613cc6a242dcec5a2fbefd45389
Sajjad <real.fisae@gmail.com>
2020-12-26 23:41:13 +0330

    Create 3 objects of Mystring class

--- main.cpp
+++ main.cpp
@@ -13,6 +13,9 @@ using namespace std;
 
 int main(int argc, char** argv) {
     
+    Mystring empty; // Creates an empty str (point to nullptr)
+    Mystring larry("Larry"); // Call Overloaded constructor (str points to "Larry")
+    Mystring stooge {larry}; // Copy constructor (new str is separate from orig.str)
     
     return 0;
 }
